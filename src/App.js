dc94cbdc4b0e724b96fb637707b02d0eeb00e0d3
Nathan Lourenço <nathanmlf@hotmail.com>
2025-04-25 19:25:35 -0300

    códigos para dar continuidade ao projeto

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+"use client";
+
+import "./App.css";
+import React from "react";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <main>
+        <div className="container-bg-images">
+          <div className="bg-image-1"></div>
+          <div className="bg-image-2"></div>
+          <div className="bg-image-3"></div>
+        </div>
+
+        <div className="main-container">
+          <img className="github-logo" src="" alt=""></img>
+          <h1 className="main-container-text">Hello!</h1>
+        </div>
+        <div>
+          <input
+            className="search-input"
+            name="myInput"
+            placeholder="Digite um usuário do Github"
+          />
+          <button className="search-input-button" type="search"></button>
+        </div>
+      </main>
+    </>
   );
 }
 
 export default App;
+
+// Container código de Erro
+
+{
+  /* <div className="container-erro">
+  <p className="container-erro-texto">
+    Nenhum perfil foi encontrado com esse nome de usuário. Tente novamente
+  </p>
+</div>;
+
+// Container quando encontra o perfil
+
+<div className="container-encontrou">
+  
+  <img src="link da foto de perfil">
+  <h2>Nome da pessoa</h2>
+  <p>Bio da pessoa</p>
+</div>; */
+}
